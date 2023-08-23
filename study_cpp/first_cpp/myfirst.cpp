b59c94832d236188606f62d9b2f85f2b0393558c
xifeng <zaijun.wzj@alibaba-inc.com>
2023-08-23 15:07:04 +0800

    myfirst cpp demo

---
+++ study_cpp/first_cpp/myfirst.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main() 
+{
+    using namespace std; // 编译指令
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    cout << "something done!" << endl;
+    std::string sd = "ssssss";
+    cout << sd << endl;
+
+    int carrots = 25;
+    cout << "I have ";
+    cout << carrots;
+    cout << " carrots;" << endl;
+
+    carrots = carrots - 1;
+
+    cout << "Crunch, Crunch. Now I have " << carrots << " carrots." << endl;
+
+    return 0;
+}
